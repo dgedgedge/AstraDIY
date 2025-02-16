eafc60391c04e5162c6cc0604d0e67dee8ef35eb
dgedgedge <denis.germa@free.fr>
2025-02-16 20:52:57 +0100

    Back to configuration at each reading to work out the ina "reset" in case f low power

--- Software/pythonDrivers/AstraIna.py
+++ Software/pythonDrivers/AstraIna.py
@@ -192,12 +192,11 @@ class AstraIna:
         if self.configurationSend:
             curtimeS=time.perf_counter()
             deltatimeS=curtimeS-self._lasttimeS
-            if self.ina219.is_conversion_ready():
-                if not self.ina219.current_overflow():
-                    self._shuntVoltagemV = self.ina219.shunt_voltage()
-                    self._voltageV = float(self.ina219.voltage())
-                    self._currentmA = float(self.ina219.current())
-                    self._powermW = float(self.ina219.power())
+            if not self.ina219.current_overflow():
+                self._shuntVoltagemV = self.ina219.shunt_voltage()
+                self._voltageV = float(self.ina219.voltage())
+                self._currentmA = float(self.ina219.current())
+                self._powermW = float(self.ina219.power())
             energiemWS=self._powermW * deltatimeS
             self._energiemWS += energiemWS
             self._lasttimeS=curtimeS
