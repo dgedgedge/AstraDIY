58ee789b402a692142ca93cf7c949e15266f7ce5
dgedgedge <denis.germa@free.fr>
2026-03-28 00:14:50 +0100

    Correction install

--- Software/Install/install_scripts.sh
+++ Software/Install/install_scripts.sh
@@ -34,6 +34,7 @@ for FILE in Astra*.py ina219.py syspwm.py bme280_lib.py survDateOffset.py
 do
 	cp ${HOMEDIR}/pythonDrivers/${FILE} $INSTALLDIR
 done
+cp -r ${HOMEDIR}/pythonDrivers/lib $INSTALLDIR/pythonDrivers
 chmod a+rx  $INSTALLDIR/Astra*.py
 chmod og-w $INSTALLDIR/Astra*.py
 
